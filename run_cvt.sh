4be5308a6c579d3f1e4beddf05d78aeb95110a71
Prashanth Palaniappan <prashanth.palaniappan@amd.com>
2024-04-16 00:46:40 +0000

    update run script to collect configs

--- run_cvt.sh
+++ run_cvt.sh
@@ -23,22 +23,18 @@ DLM_RUNTIME_NGPUS=8
 export HIP_FORCE_DEV_KERNARG=1
 #clear checkpoints
 rm -rf OUTPUT
-# enable tunable ops
-# export PYTORCH_TUNABLEOP_ENABLED=1
-# warmup run with 1 epoch to generate tunable op solutions
-export MIOPEN_ENABLE_LOGGING=1
-export MIOPEN_ENABLE_LOGGING_CMD=1
-export MIOPEN_LOG_LEVEL=6
-MASTER_ADDR=$MASTER_ADDR NODE_COUNT=$NODE_COUNT RANK=$RANK bash run_cvt_train.sh $DLM_RUNTIME_NGPUS $DLM_MODEL_BATCH_SIZE 1 2>&1 | tee log.1machine.miopenconfig.txt
-unset MIOPEN_ENABLE_LOGGING
-unset MIOPEN_ENABLE_LOGGING_CMD
-unset MIOPEN_LOG_LEVEL
-# disable further tuning
-# export PYTORCH_TUNABLEOP_TUNING=0
-#clear checkpoints
-# rm -rf OUTPUT
-# final performance run
+# profiling is set to NONE by default. Change it in benchmarks/cvt/ootb/CvT/tools/train.py for PyTorch or RPD tracing.
 MASTER_ADDR=$MASTER_ADDR NODE_COUNT=$NODE_COUNT RANK=$RANK bash run_cvt_train.sh $DLM_RUNTIME_NGPUS $DLM_MODEL_BATCH_SIZE $DLM_MODEL_NUM_EPOCHS 2>&1 | tee log.1machine.txt
+
+# collect configs for 1 epoch
+rm -rf OUTPUT
+export MIOPEN_ENABLE_LOGGING_CMD=1
+#export MIOPEN_LOG_LEVEL=6
+export ROCBLAS_LAYER=6
+MASTER_ADDR=$MASTER_ADDR NODE_COUNT=$NODE_COUNT RANK=$RANK bash run_cvt_train.sh $DLM_RUNTIME_NGPUS $DLM_MODEL_BATCH_SIZE 1 2>&1 | tee log.1machine.configs.txt
+unset MIOPEN_ENABLE_LOGGING_CMD
+#unset export MIOPEN_LOG_LEVEL
+unset ROCBLAS_LAYER
 
 #val_1gpu=$(cat "log.1gpu.txt" | grep -oP "'performance', \K.*(?=\),)")
 val_1machine=$(cat "log.1machine.txt" | grep -oP "'performance', \K.*(?=\),)")
