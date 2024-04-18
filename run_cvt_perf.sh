56030caa978c85aa66195838e30339d37de1f8d2
Prashanth Palaniappan <prashanth.palaniappan@amd.com>
2024-04-18 19:18:21 +0000

    remove unnecessary lines

--- run_cvt_perf.sh
+++ run_cvt_perf.sh
@@ -23,12 +23,10 @@ rm -rf OUTPUT
 # profiling is set to NONE by default. Change it in benchmarks/cvt/ootb/CvT/tools/train.py for PyTorch or RPD tracing.
 MASTER_ADDR=$MASTER_ADDR NODE_COUNT=$NODE_COUNT RANK=$RANK bash run_cvt_train.sh $DLM_RUNTIME_NGPUS $DLM_MODEL_BATCH_SIZE $DLM_MODEL_NUM_EPOCHS 2>&1 | tee log.1machine.txt
 
-#val_1gpu=$(cat "log.1gpu.txt" | grep -oP "'performance', \K.*(?=\),)")
 val_1machine=$(cat "log.1machine.txt" | grep -oP "'performance', \K.*(?=\),)")
 metric="samples/s"
 
 echo "model,performance,metric" > results_cvt.csv
-#echo "1gpu,$val_1gpu,$metric" >> results_cvt.csv
 echo "1machine,$val_1machine,$metric" >> results_cvt.csv
 cp -v results_cvt.csv ../../../../
 echo "performance: $val_1machine $metric"
